09d22a415632f5485d725d1afafb438a5df4337e
ravirizkhan7 <razkhan070405@gmail.com>
2025-04-28 15:10:18 +0700

    tugas pertemuan 3

---
+++ pertemuan3/pertemuan3.js
@@ -0,0 +1,12 @@
+console.log("Hello Word");
+console.log('Selamat datang di kelas JavaScript!');
+
+let kota = "Padang";
+console.log(kota);
+
+// perkenalan diri
+//buat codingan memakai const atau let
+nama
+nim 
+umur
+alamat
